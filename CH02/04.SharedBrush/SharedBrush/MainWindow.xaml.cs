180b0a71327ae159276876a6f56625e614d0ef09
bobchin <bobchin.ryu@gmail.com>
2016-02-29 17:49:38 +0900

    SharedBrushを作成

---
+++ CH02/04.SharedBrush/SharedBrush/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SharedBrush
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // ソースから追加することも可能
+            this.Resources.Add("fontFamily2", new FontFamily("Times New Roman"));
+
+            InitializeComponent();
+
+            // XAMLで指定していればこのようにリソースが参照できる
+            FontFamily fntfam = this.Resources["fontFamily"] as FontFamily;
+
+            // リソースは共有されるか？
+            /*
+            TextBlock txtblk = (this.Content as Grid).Children[1] as TextBlock;
+            LinearGradientBrush brush = txtblk.Foreground as LinearGradientBrush;
+            brush.StartPoint = new Point(0, 1);
+            brush.EndPoint = new Point(0, 0);
+            */
+        }
+    }
+}
