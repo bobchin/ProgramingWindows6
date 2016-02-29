6158ebdce23db11ba056d814f4e4c5da332ddae7
bobchin <bobchin.ryu@gmail.com>
2016-02-29 09:12:37 +0900

    HelloImageCodeを作成

---
+++ CH01/HelloImageCode/HelloImageCode/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloImageCode
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Uri uri = new Uri("http://www.charlespetzold.com/pw6/PetzoldJersey.jpg");
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image image = new Image();
+            image.Source = bitmap;
+
+            // Grid の Name を使う場合
+            // contentGrid.Children.Add(image);
+
+            // Window の Content を使う場合
+            /*
+            Grid grid = this.Content as Grid;
+            grid.Children.Add(image);
+            */
+
+            // Grid を削除してしまう場合
+            this.Content = image;
+        }
+    }
+}
