ced4becc0baff41c98e61271b2b1bb8c7f38f05c
bobchin <bobchin.ryu@gmail.com>
2016-03-02 17:25:00 +0900

    ImageBrushedSpiralを作成

---
+++ CH02/07.ImageBrushedSpiral/ImageBrushedSpiral/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageBrushedSpiral
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int angle = 0; angle < 3600; angle++)
+            {
+                double radians = Math.PI * angle / 180;
+                double radius = angle / 3.6;
+                double x = 1000 + radius * Math.Sin(radians);
+                double y = 1000 - radius * Math.Cos(radians);
+
+                polyline.Points.Add(new Point(x, y));
+            }
+        }
+    }
+}
