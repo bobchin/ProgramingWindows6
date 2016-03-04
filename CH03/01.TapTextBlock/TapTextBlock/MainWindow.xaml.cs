12f9cb2f0ac630e54b40423f0672b1d27d71926f
bobchin <bobchin.ryu@gmail.com>
2016-03-04 16:08:04 +0900

    TapTextBlockを作成

---
+++ CH03/01.TapTextBlock/TapTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace TapTextBlock
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtblk_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+    }
+}
