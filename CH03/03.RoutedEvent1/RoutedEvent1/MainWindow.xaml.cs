4798e525715c71c76d97e70390e43fdbd52922d1
bobchin <bobchin.ryu@gmail.com>
2016-03-08 13:38:30 +0900

    RoutedEvent1を作成

---
+++ CH03/03.RoutedEvent1/RoutedEvent1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvent1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnTextBlockMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            if (!(args.OriginalSource is TextBlock))
+            {
+                return;
+            }
+
+            TextBlock txtblk = args.OriginalSource as TextBlock;
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+    }
+}
