f1729a6cf608cd0625f30be9b70063dfc716b2a8
bobchin <bobchin.ryu@gmail.com>
2016-03-08 14:18:43 +0900

    RoutedEvent5を作成

---
+++ CH03/07.RoutedEvent5/RoutedEvent5/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RoutedEvent5
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnTextBlockMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            (sender as TextBlock).Foreground = GetRandomBrush();
+
+            // イベントを上位の要素に伝えない
+            args.Handled = true;
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            contentGrid.Background = GetRandomBrush();
+            base.OnMouseDown(e);
+        }
+
+        Brush GetRandomBrush()
+        {
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            return new SolidColorBrush(clr);
+        }
+    }
+}
