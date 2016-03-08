a7ea4b165e89e98dfcbfc62594f8b618d89dc5e5
bobchin <bobchin.ryu@gmail.com>
2016-03-08 14:29:39 +0900

    RoutedEvent6を作成

---
+++ CH03/08.RoutedEvent6/RoutedEvent6/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RoutedEvent6
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AddHandler(
+                UIElement.MouseDownEvent,
+                new MouseButtonEventHandler(OnWindowMouseDown),
+                true);
+        }
+
+        void OnTextBlockMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            (sender as TextBlock).Foreground = GetRandomBrush();
+
+            // イベントを上位の要素に伝えない
+            args.Handled = true;
+        }
+
+        void OnWindowMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            contentGrid.Background = GetRandomBrush();
+        }
+
+        Brush GetRandomBrush()
+        {
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            return new SolidColorBrush(clr);
+        }
+    }
+}
