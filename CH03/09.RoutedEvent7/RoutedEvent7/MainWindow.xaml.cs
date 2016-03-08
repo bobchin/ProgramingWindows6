4f061a12c43c9cfbea520f8f6d03645428f6ebd8
bobchin <bobchin.ryu@gmail.com>
2016-03-08 14:43:17 +0900

    RoutedEvent7を作成

---
+++ CH03/09.RoutedEvent7/RoutedEvent7/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvent7
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            SolidColorBrush brush = new SolidColorBrush(clr);
+
+            if (e.OriginalSource is TextBlock)
+            {
+                (e.OriginalSource as TextBlock).Foreground = brush;
+            }
+            else if (e.OriginalSource is Grid)
+            {
+                (e.OriginalSource as Grid).Background = brush;
+            }
+
+            base.OnMouseDown(e);
+        }
+    }
+}
