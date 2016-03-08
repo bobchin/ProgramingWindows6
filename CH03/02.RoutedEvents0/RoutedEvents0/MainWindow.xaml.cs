aa3cc0c0818f371768e0cc44fce5357b1d58ba87
bobchin <bobchin.ryu@gmail.com>
2016-03-08 13:30:27 +0900

    RoutedEvents0を作成

---
+++ CH03/02.RoutedEvents0/RoutedEvents0/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RoutedEvents0
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnTextBlockMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            TextBlock txtblk = sender as TextBlock;
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+    }
+}
