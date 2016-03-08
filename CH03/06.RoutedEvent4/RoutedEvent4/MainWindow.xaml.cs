54a10b550dc215d18b5a85ce4c7eeb9ff03da7c7
bobchin <bobchin.ryu@gmail.com>
2016-03-08 14:13:57 +0900

    RoutedEvent4を作成

---
+++ CH03/06.RoutedEvent4/RoutedEvent4/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvent4
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnTextBlockMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            (sender as TextBlock).Foreground = GetRandomBrush();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            contentGrid.Background = GetRandomBrush();
+            base.OnMouseDown(e);
+        }
+
+        Brush GetRandomBrush()
+        {
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            return new SolidColorBrush(clr);
+        }
+    }
+}
