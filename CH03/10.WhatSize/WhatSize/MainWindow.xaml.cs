4295adfe1d7ed6d1e4daa176bdcb932c2c9ce493
bobchin <bobchin.ryu@gmail.com>
2016-03-08 15:00:42 +0900

    WhatSizeを作成

---
+++ CH03/10.WhatSize/WhatSize/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WhatSize
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // XAMLで指定せずここで指定してもよい
+            // this.SizeChanged += OnWindowSizeChanged;
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            /*
+            widthText.Text = e.NewSize.Width.ToString();
+            heightText.Text = e.NewSize.Height.ToString();
+            */
+            widthText.Text = this.ActualWidth.ToString();
+            heightText.Text = this.ActualHeight.ToString();
+        }
+    }
+}
