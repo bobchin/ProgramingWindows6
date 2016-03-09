5ebb05f10ac43e7a73cd4ab00bdb069cba4a1f9a
bobchin <bobchin.ryu@gmail.com>
2016-03-09 17:46:01 +0900

    ManualColorAnimationを作成

---
+++ CH03/15.ManualColorAnimation/ManualColorAnimation/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManualColorAnimation
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CompositionTarget.Rendering += OnCompositionTargetRendering;
+        }
+
+        private void OnCompositionTargetRendering(object sender, EventArgs args)
+        {
+            RenderingEventArgs renderingArgs = args as RenderingEventArgs;
+            double t = (0.25 * renderingArgs.RenderingTime.TotalSeconds) % 1;
+            t = (t < 0.5) ? 2 * t : 2 - 2 * t;
+
+            // 背景
+            byte gray = (byte)(255 * t);
+            gridBrush.Color = Color.FromArgb(255, gray, gray, gray);
+
+            // 前景
+            gray = (byte)(255 - gray);
+            txtblkBrush.Color = Color.FromArgb(255, gray, gray, gray);
+        }
+    }
+}
