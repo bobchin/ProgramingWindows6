07d5d8c835ba1375f49deccfa15a84cab63259ff
bobchin <bobchin.ryu@gmail.com>
2016-03-14 11:59:04 +0900

    ColorList2を作成

---
+++ CH04/13.ColorList2/ColorList2/ColorItem.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorList2
+{
+    /// <summary>
+    /// ColorItem.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ColorItem : UserControl
+    {
+        // パラメータなしのコンストラクタがないので
+        // XAMLでインスタンス化することはできないので注意！
+        public ColorItem(string name, Color clr)
+        {
+            InitializeComponent();
+
+            rectangle.Fill = new SolidColorBrush(clr);
+            txtblkName.Text = name;
+            txtblkRgb.Text = String.Format("{0:X2}-{1:X2}-{2:X2}-{3:X2}", clr.A, clr.R, clr.G,clr.B);
+        }
+    }
+}
---
+++ CH04/13.ColorList2/ColorList2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorList2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainLogic();
+        }
+
+        private void MainLogic()
+        {
+            IEnumerable<PropertyInfo> properties = typeof(Colors).GetTypeInfo().DeclaredProperties;
+            foreach (PropertyInfo property in properties)
+            {
+                Color clr = (Color)property.GetValue(null);
+                ColorItem clrItem = new ColorItem(property.Name, clr);
+
+                stackPanel.Children.Add(clrItem);
+            }
+        }
+    }
+}
