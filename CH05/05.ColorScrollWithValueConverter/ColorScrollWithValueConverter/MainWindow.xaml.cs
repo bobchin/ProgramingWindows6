e2e1a2f7847fafd6f92813ce56e406dd466453cb
bobchin <bobchin.ryu@gmail.com>
2016-03-17 17:59:46 +0900

    ColorScrollWithValueConverterを作成

---
+++ CH05/05.ColorScrollWithValueConverter/ColorScrollWithValueConverter/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorScrollWithValueConverter
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte r = (byte)redSlider.Value;
+            byte g = (byte)greenSlider.Value;
+            byte b = (byte)blueSlider.Value;
+
+            brushResult.Color = Color.FromArgb(255, r, g, b);
+        }
+    }
+
+    public class DoubleToStringHexByteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)(double)value).ToString("X2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
