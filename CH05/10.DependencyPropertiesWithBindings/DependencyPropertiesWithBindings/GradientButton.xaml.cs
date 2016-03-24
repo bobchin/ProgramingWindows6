b695372ba7f5aac37874a0e4fc951d9bd68a78ad
bobchin <bobchin.ryu@gmail.com>
2016-03-24 14:41:45 +0900

    DependencyPropertiesWithBindingsを作成

---
+++ CH05/10.DependencyPropertiesWithBindings/DependencyPropertiesWithBindings/GradientButton.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DependencyPropertiesWithBindings
+{
+    /// <summary>
+    /// GradientButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class GradientButton : Button
+    {
+        public static DependencyProperty Color1Property { private set; get; }
+        public static DependencyProperty Color2Property { private set; get; }
+
+        static GradientButton()
+        {
+            Color1Property =
+                DependencyProperty.Register("Color1",
+                    typeof(Color),
+                    typeof(GradientButton),
+                    new PropertyMetadata(Colors.White));
+            Color2Property =
+                DependencyProperty.Register("Color2",
+                    typeof(Color),
+                    typeof(GradientButton),
+                    new PropertyMetadata(Colors.Black));
+        }
+
+        public GradientButton()
+        {
+            InitializeComponent();
+        }
+
+        public Color Color1
+        {
+            set { SetValue(Color1Property, value); }
+            get { return (Color)GetValue(Color1Property); }
+        }
+
+        public Color Color2
+        {
+            set { SetValue(Color2Property, value); }
+            get { return (Color)GetValue(Color2Property); }
+        }
+    }
+}
