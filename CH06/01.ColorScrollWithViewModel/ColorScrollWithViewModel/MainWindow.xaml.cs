657aef8e5ef60260038e60968c68f438c8328586
bobchin <bobchin.ryu@gmail.com>
2016-03-25 11:25:51 +0900

    ColorScrollWithViewModelを作成

---
+++ CH06/01.ColorScrollWithViewModel/ColorScrollWithViewModel/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorScrollWithViewModel
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class DoubleToStringHexByteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)(double)value).ToString("X2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ CH06/01.ColorScrollWithViewModel/ColorScrollWithViewModel/RgbViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorScrollWithViewModel
+{
+    public class RgbViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        double red, green, blue;
+        Color color = Color.FromArgb(255, 0, 0, 0);
+
+        public double Red
+        {
+            set
+            {
+                if (red != value)
+                {
+                    red = value;
+                    OnProperyChanged("Red");
+                    Calculate();
+                }
+            }
+
+            get
+            {
+                return red;
+            }
+        }
+
+        public double Green
+        {
+            set
+            {
+                if (green != value)
+                {
+                    green = value;
+                    OnProperyChanged("Green");
+                    Calculate();
+                }
+            }
+
+            get
+            {
+                return green;
+            }
+        }
+
+        public double Blue
+        {
+            set
+            {
+                if (blue != value)
+                {
+                    blue = value;
+                    OnProperyChanged("Blue");
+                    Calculate();
+                }
+            }
+
+            get
+            {
+                return blue;
+            }
+        }
+
+        public Color Color
+        {
+            /*
+               安全のためクラス内部からしか変更できないようにprotectedにしておく
+             */
+            protected set
+            {
+                if (color != value)
+                {
+                    color = value;
+                    OnProperyChanged("Color");
+                }
+            }
+            /*
+            set
+            {
+                if (color != value)
+                {
+                    color = value;
+                    OnProperyChanged("Color");
+                    this.Red = value.R;
+                    this.Green = value.G;
+                    this.Blue = value.B;
+                }
+            }
+            */
+            get
+            {
+                return color;
+            }
+        }
+
+        private void OnProperyChanged(string properyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(properyName));
+            }
+        }
+
+        private void Calculate()
+        {
+            this.Color = Color.FromArgb(255, (byte)this.Red, (byte)this.Green, (byte)this.Blue);
+        }
+
+    }
+}
