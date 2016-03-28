5ebfcaf365b8167acd4863fb75e13957ab996b3d
bobchin <bobchin.ryu@gmail.com>
2016-03-28 18:51:45 +0900

    ColorTextBoxesを作成

---
+++ CH06/03.ColorTextBoxes/ColorTextBoxes/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorTextBoxes
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainLogic();
+        }
+
+        private void MainLogic()
+        {
+            this.DataContext = new RgbViewModel();
+
+            // ハイライト色に初期化
+            (this.DataContext as RgbViewModel).Color = SystemColors.HighlightColor;
+        }
+    }
+
+}
