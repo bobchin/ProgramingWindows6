78b40c896fa24ba7c577b4c9937376ce220f1841
bobchin <bobchin.ryu@gmail.com>
2016-03-29 15:36:39 +0900

    HowToAsync1を作成

---
+++ CH07/01.HowToAsync1/HowToAsync1/DialogButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToAsync1
+{
+    public class DialogButton
+    {
+        public DialogButton(string title, object color)
+        {
+            this.Title = title;
+            this.Id = color;
+        }
+
+        public string Title { set; get; }
+        public object Id { set; get; }
+    }
+}
---
+++ CH07/01.HowToAsync1/HowToAsync1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HowToAsync1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Color clr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogButton[] buttons = new DialogButton[]
+            {
+                new DialogButton("Red", Colors.Red),
+                new DialogButton("Green", Colors.Green),
+                new DialogButton("Blue", Colors.Blue),
+            };
+            MessageDialog m = new MessageDialog("Choose a color", "How To Async #1", buttons);
+
+            Task<Object> messageTask = m.ShowAsync();
+            messageTask.ContinueWith((task) =>
+            {
+                if (task.Result == null)
+                {
+                    return;
+                }
+                clr = (Color)task.Result;
+                Dispatcher.BeginInvoke(new Action(() => { OnDispatcherRunAsyncCallback(); }), null);
+            });
+        }
+
+        private void OnDispatcherRunAsyncCallback()
+        {
+            // 背景をセット
+            contentGrid.Background = new SolidColorBrush(clr);
+        }
+    }
+}
