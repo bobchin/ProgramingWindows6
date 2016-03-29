9023bbc15cf68a3dc2f5f6a69e810f460b0ea98f
bobchin <bobchin.ryu@gmail.com>
2016-03-29 16:37:25 +0900

    HowToAsync3を作成

---
+++ CH07/03.HowToAsync3/HowToAsync3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HowToAsync3
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Color clr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogButton[] buttons = new DialogButton[]
+            {
+                new DialogButton("Red", Colors.Red),
+                new DialogButton("Green", Colors.Green),
+                new DialogButton("Blue", Colors.Blue),
+            };
+            MessageDialog m = new MessageDialog("Choose a color", "How To Async #2", buttons);
+
+            object result = await m.ShowAsync();
+            if (result == null)
+            {
+                return;
+            }
+            clr = (Color)result;
+            contentGrid.Background = new SolidColorBrush(clr);
+        }
+
+    }
+}
