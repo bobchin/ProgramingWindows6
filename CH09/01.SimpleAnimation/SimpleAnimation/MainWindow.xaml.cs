0e2f7c2f4334187c7d7978b9c82086e76ac5996b
bobchin <bobchin.ryu@gmail.com>
2016-04-25 13:38:31 +0900

    SimpleAnimationを作成

---
+++ CH09/01.SimpleAnimation/SimpleAnimation/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SimpleAnimation
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            (this.Resources["storyboard"] as Storyboard).Begin();
+        }
+    }
+}
