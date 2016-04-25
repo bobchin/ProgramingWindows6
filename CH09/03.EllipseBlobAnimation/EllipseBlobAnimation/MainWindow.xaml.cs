92570c313441084976d9332b441d3c1f56f18e06
bobchin <bobchin.ryu@gmail.com>
2016-04-25 14:01:41 +0900

    EllipseBlobAnimationを作成

---
+++ CH09/03.EllipseBlobAnimation/EllipseBlobAnimation/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace EllipseBlobAnimation
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                (this.Resources["storyboard"] as Storyboard).Begin();
+            };
+        }
+    }
+}
