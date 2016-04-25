23ae9824c6d0965f1029da6ca37aab84370e3d2a
bobchin <bobchin.ryu@gmail.com>
2016-04-25 14:22:12 +0900

    AnimateDashOffsetを作成

---
+++ CH09/05.AnimateDashOffset/AnimateDashOffset/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace AnimateDashOffset
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            (this.Resources["storyboard"] as Storyboard).Begin();
+        }
+    }
+}
