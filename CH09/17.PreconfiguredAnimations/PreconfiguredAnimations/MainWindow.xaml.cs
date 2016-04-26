44f170f48bd3265f38681df764a7f891e164726b
bobchin <bobchin.ryu@gmail.com>
2016-04-26 17:21:37 +0900

    PreconfiguredAnimationsを作成

---
+++ CH09/17.PreconfiguredAnimations/PreconfiguredAnimations/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace PreconfiguredAnimations
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string key = btn.Tag as string;
+            Storyboard storyboard = this.Resources[key] as Storyboard;
+            storyboard.Begin();
+        }
+    }
+}
