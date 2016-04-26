e9a9ccc6100610a616854beac30aeeba06b34534
bobchin <bobchin.ryu@gmail.com>
2016-04-26 17:57:17 +0900

    RotateTheTextを作成

---
+++ CH10/02.RotateTheText/RotateTheText/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RotateTheText
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /*
+            Loaded += (sender, args) =>
+            {
+                RotateTransform rotate = txtblk.RenderTransform as RotateTransform;
+                rotate.CenterX = txtblk.ActualWidth / 2;
+                rotate.CenterY = txtblk.ActualHeight / 2;
+            };
+            */
+        }
+    }
+}
