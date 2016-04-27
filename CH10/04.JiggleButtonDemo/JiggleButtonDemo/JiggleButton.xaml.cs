91a49e70c2b42bcfdaf79ce27c14dd087b28579e
bobchin <bobchin.ryu@gmail.com>
2016-04-27 09:23:38 +0900

    JiggleButtonDemoを作成

---
+++ CH10/04.JiggleButtonDemo/JiggleButtonDemo/JiggleButton.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace JiggleButtonDemo
+{
+    /// <summary>
+    /// JiggleButton.xaml の相互作用ロジック
+    /// </summary>
+    public sealed partial class JiggleButton : Button
+    {
+        public JiggleButton()
+        {
+            InitializeComponent();
+        }
+
+        private void OnJiggleClick(object sender, RoutedEventArgs e)
+        {
+            (this.Resources["jiggleAnimation"] as Storyboard).Begin();
+        }
+    }
+}
