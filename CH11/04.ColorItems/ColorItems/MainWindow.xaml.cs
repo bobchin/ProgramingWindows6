44007296ffc407c413d51432e297eb18b9bfecf6
bobchin <bobchin.ryu@gmail.com>
2016-05-09 14:01:47 +0900

    ColorItemsを作成

---
+++ CH11/04.ColorItems/ColorItems/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorItems
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainLogic();
+        }
+
+        private void MainLogic()
+        {
+            IEnumerable<PropertyInfo> properties = typeof(Colors).GetTypeInfo().DeclaredProperties;
+            foreach (PropertyInfo property in properties)
+            {
+                Color clr = (Color)property.GetValue(null);
+                itemsControl.Items.Add(clr);
+            }
+        }
+    }
+}
