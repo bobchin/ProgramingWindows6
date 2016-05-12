2371335d8892eb7e6f5f16d7b1f365577bc18435
bobchin <bobchin.ryu@gmail.com>
2016-05-12 09:56:20 +0900

    HorizontalListBoxを作成

---
+++ CH11/09.HorizontalListBox/HorizontalListBox/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Petzold.ProgrammingWindows6.Chapter11;
+
+namespace HorizontalListBox
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnItemLoaded(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Item Loaded" +
+                ((sender as FrameworkElement).DataContext as NamedColor).Name);
+        }
+    }
+}
